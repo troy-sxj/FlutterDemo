02095b08f7e16d1904224197c9e8a1ae0add4c4f
shangxiaojian <shangxiaojian@pptv.com>
2018-07-05 14:12:27 +0800

    [Feature] Image Widget使用

--- lib/cookbook/ThemeDemo.dart
+++ lib/cookbook/ThemeDemo.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:transparent_image/transparent_image.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class ThemeDemo extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     final appName = "Theme Demo";
@@ -12,17 +13,17 @@ class ThemeDemo extends StatelessWidget {
         primaryColor: Colors.lightBlue[500],
         accentColor: Colors.cyan[500],
       ),
-      home: new MyHomePage(title: appName,),
+      home: new MyHomePage(
+        title: appName,
+      ),
     );
   }
-
 }
 
 class MyHomePage extends StatelessWidget {
-
   final String title;
 
-  MyHomePage({Key key, @required this.title}) :super(key: key);
+  MyHomePage({Key key, @required this.title}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -30,23 +31,26 @@ class MyHomePage extends StatelessWidget {
       appBar: new AppBar(
         title: Text(title),
       ),
-      body: new Center(
-        child: new Container(
-          color: Theme
-              .of(context)
-              .accentColor,
-          child: new Text("Text with background color",
-            style: Theme
-                .of(context)
-                .textTheme
-                .title,),
-        ),
+      body: new Stack(
+        children: <Widget>[
+          new Center(
+            child: new CircularProgressIndicator(),
+          ),
+          new Center(
+            child: new CachedNetworkImage(
+              placeholder: new CircularProgressIndicator(),
+              imageUrl:
+                  'https://github.com/flutter/website/blob/master/_includes/code/layout/lakes/images/lake.jpg?raw=true',
+            ),
+          )
+        ],
       ),
       floatingActionButton: new Theme(
           data: Theme.of(context).copyWith(accentColor: Colors.yellow),
           child: new FloatingActionButton(
-            onPressed: null, child: new Icon(Icons.add),)),
+            onPressed: null,
+            child: new Icon(Icons.add),
+          )),
     );
   }
-
 }
